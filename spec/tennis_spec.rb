f8e184cd6dbed74d6907820b3713a6c521938dca
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-29 12:53:11 +0100

    Refactoring tennis_spec

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -3,22 +3,22 @@
 
 require 'tennis'
   describe TennisScorer do
+    let(:tennis_scorer) { TennisScorer.new }
     it "can score a new game with 0 points" do
-      tennis_scorer = TennisScorer.new
       score = tennis_scorer.score
       expect(tennis_scorer.score).to eq('love - love') # player 1 and player 2 are 0 - 0
     end
 
     it "can score a game where player one has scored once" do
-      tennis_scorer = TennisScorer.new
       tennis_scorer.win_point
       expect(tennis_scorer.score).to eq('fifteen - love')
     end
 
     it "can score a game where player one has scored twice" do
-      tennis_scorer = TennisScorer.new
       tennis_scorer.win_point
       tennis_scorer.win_point
       expect(tennis_scorer.score).to eq('thirty - love')
     end
+  
+
   end
